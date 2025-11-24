355c9d6e182ef84d84bbd5571c81f76a0b874a31
youssefElhoubi <elhoubiyoussef@gmail.com>
2025-11-24 11:32:48 +0100

    feat: made user creation method

--- src/main/java/com/smartshop/dto/user/CreateUserDto.java
+++ src/main/java/com/smartshop/dto/user/CreateUserDto.java
@@ -3,7 +3,13 @@ package com.smartshop.dto.user;
 import com.smartshop.enums.Role;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import lombok.*;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
 public class CreateUserDto {
     @NotEmpty(message = "username should not be empty")
     @NotBlank(message = "username should not be blank")
--- src/main/java/com/smartshop/service/UserService.java
+++ src/main/java/com/smartshop/service/UserService.java
@@ -1,5 +1,7 @@
 package com.smartshop.service;
 
+import com.smartshop.config.PasswordUtil;
+import com.smartshop.dto.user.CreateUserDto;
 import com.smartshop.entity.User;
 import com.smartshop.mapper.UserMapper;
 import com.smartshop.repository.UserRepository;
@@ -9,11 +11,22 @@ import org.springframework.stereotype.Service;
 public class UserService {
     private UserRepository userRepository;
     private UserMapper userMapper;
+    private PasswordUtil passwordUtil;
 
-    public UserService(UserRepository userRepository, UserMapper userMapper) {
+    public UserService(UserRepository userRepository, UserMapper userMapper, PasswordUtil passwordUtil) {
         this.userRepository = userRepository;
         this.userMapper = userMapper;
+        this.passwordUtil = passwordUtil;
     }
 
-//    public User create()
+    public User create(CreateUserDto dto) {
+        User user = userRepository.findByUserName(dto.getUsername());
+        if (user != null) {
+            throw new RuntimeException("user already exist");
+        }
+        user = userMapper.toEntity(dto);
+        user.setPassword(passwordUtil.passwordHash(dto.getPassword()));
+        userRepository.save(user);
+        return user;
+    }
 }
