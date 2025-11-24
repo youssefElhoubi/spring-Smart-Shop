85614dc005c54a01b4aee317b4ba77b7c118ba51
youssefElhoubi <elhoubiyoussef@gmail.com>
2025-11-24 10:24:51 +0100

    feat:made the user entity

---
+++ src/main/java/com/smartshop/entity/User.java
@@ -0,0 +1,25 @@
+package com.smartshop.entity;
+
+import com.smartshop.enums.Role;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String userName;
+    private String password;
+    private Role role;
+}
---
+++ src/main/java/com/smartshop/enums/Role.java
@@ -0,0 +1,6 @@
+package com.smartshop.enums;
+
+public enum Role {
+    ADMIN,
+    CLIENT
+}
