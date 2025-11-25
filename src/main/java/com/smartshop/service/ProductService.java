20eca8a069c8d6e94be5062eff955503c7ba06ad
youssefElhoubi <elhoubiyoussef@gmail.com>
2025-11-25 14:15:27 +0100

    Added ProductService class with CRUD operations for products

--- src/main/java/com/smartshop/service/ProductService.java
+++ src/main/java/com/smartshop/service/ProductService.java
@@ -1,4 +1,41 @@
 package com.smartshop.service;
 
+import com.smartshop.dto.Product.ProductRequestDTO;
+import com.smartshop.dto.Product.ProductResponseDTO;
+import com.smartshop.entity.Product;
+import com.smartshop.exeptions.ResourceNotFoundException;
+import com.smartshop.mapper.ProductMapper;
+import com.smartshop.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ProductService {
+    private final ProductRepository productRepository;
+    private final ProductMapper productMapper;
+
+    public ProductService(ProductRepository productRepository, ProductMapper productMapper) {
+        this.productRepository = productRepository;
+        this.productMapper = productMapper;
+    }
+
+    public ProductResponseDTO findById(Long id) {
+        Product product = productRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Product not found with ID: " + id));
+        return productMapper.toResponseDTO(product);
+    }
+
+    public List<ProductResponseDTO> findAllActive() {
+        List<Product> activeProducts = productRepository.findByDeletedFalse();
+        return activeProducts.stream()
+                .map(productMapper::toResponseDTO)
+                .toList();
+    }
+
+    public ProductResponseDTO create(ProductRequestDTO dto) {
+        Product newProduct = productMapper.toEntity(dto);
+        Product savedProduct = productRepository.save(newProduct);
+        return productMapper.toResponseDTO(savedProduct);
+    }
 }
