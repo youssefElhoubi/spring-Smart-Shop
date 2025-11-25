e40864b7d2fc5bc4a1a0c709a595d21c5e8f2429
youssefElhoubi <elhoubiyoussef@gmail.com>
2025-11-25 09:20:54 +0100

    made a client repostory and refactored client service

--- src/main/java/com/smartshop/service/ClientService.java
+++ src/main/java/com/smartshop/service/ClientService.java
@@ -3,17 +3,18 @@ package com.smartshop.service;
 import com.smartshop.dto.client.ClientResponseDTO;
 import com.smartshop.dto.client.CreateClientDTO;
 import com.smartshop.mapper.ClientMapper;
+import com.smartshop.repository.ClientRepository;
 import com.smartshop.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ClientService {
-    private ClientService clientService;
+    private ClientRepository clientRepository;
     private ClientMapper clientMapper;
     private UserRepository userRepository;
 
-    public ClientService(ClientService clientService, ClientMapper clientMapper) {
-        this.clientService = clientService;
+    public ClientService(ClientRepository clientRepository, ClientMapper clientMapper) {
+        this.clientRepository = clientRepository;
         this.clientMapper = clientMapper;
     }
     public ClientResponseDTO createClient(CreateClientDTO dto){
