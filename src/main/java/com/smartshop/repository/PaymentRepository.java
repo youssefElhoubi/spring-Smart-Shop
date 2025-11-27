be5635c24afb0f896557c02abcad048ba50068ef
youssefElhoubi <elhoubiyoussef@gmail.com>
2025-11-27 09:50:07 +0100

    Added @Repository annotation to PaymentRepository interface

--- src/main/java/com/smartshop/repository/PaymentRepository.java
+++ src/main/java/com/smartshop/repository/PaymentRepository.java
@@ -2,6 +2,8 @@ package com.smartshop.repository;
 
 import com.smartshop.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PaymentRepository extends JpaRepository<Payment,Long> {
 }
