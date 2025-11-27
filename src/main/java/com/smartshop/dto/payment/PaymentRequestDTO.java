a52a65a708bc23eba65896aeee338d1dc9b8ea51
youssefElhoubi <elhoubiyoussef@gmail.com>
2025-11-27 09:31:26 +0100

    Added validation annotations to PaymentRequestDTO

--- src/main/java/com/smartshop/dto/payment/PaymentRequestDTO.java
+++ src/main/java/com/smartshop/dto/payment/PaymentRequestDTO.java
@@ -1,8 +1,23 @@
 package com.smartshop.dto.payment;
 
 import com.smartshop.enums.PaymentMethod;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.*;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class PaymentRequestDTO {
+    @NotNull(message = "Payment method is required")
     private PaymentMethod method;
+
+    @NotNull(message = "Order ID is required")
     private Long orderId;
+
+    @NotNull(message = "Amount is required")
+    @Positive(message = "Amount must be greater than zero")
+    private Double amount;
 }
