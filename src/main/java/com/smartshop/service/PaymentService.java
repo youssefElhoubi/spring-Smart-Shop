b108ed6f833fc8ed1b27032118dc6a99c88adef0
youssefElhoubi <elhoubiyoussef@gmail.com>
2025-11-28 10:22:13 +0100

    added a method to update pament

--- src/main/java/com/smartshop/service/PaymentService.java
+++ src/main/java/com/smartshop/service/PaymentService.java
@@ -2,6 +2,7 @@ package com.smartshop.service;
 
 import com.smartshop.dto.payment.PaymentRequestDTO;
 import com.smartshop.dto.payment.PaymentResponseDTO;
+import com.smartshop.dto.payment.PaymentUpdateDTO;
 import com.smartshop.entity.Order;
 import com.smartshop.entity.Payment;
 import com.smartshop.enums.OrderStatus;
@@ -40,12 +41,33 @@ public class PaymentService {
             throw new BusinessRuleViolationException("payment amount is bigger than remaining amount  ");
         }
         order.setRemainingAmount(order.getRemainingAmount() - payment.getAmount());
-        if(order.getRemainingAmount() == 0) {
+        if (order.getRemainingAmount() == 0) {
             order.setStatus(OrderStatus.CONFIRMED);
             payment.setPaymentStatus(PaymentStatus.ENCAISSE);
         }
         orderRepository.save(order);
         paymentRepository.save(payment);
+        return paymentMapper.toResponse(payment);
+    }
+
+    public PaymentResponseDTO updatePayment(PaymentUpdateDTO dto, Long id) {
+        Payment payment = paymentRepository.findById(id).orElseThrow(() -> {
+            throw new ResourceNotFoundException("payment with id" + id + " was not found  ");
+        });
+        if (payment.getPaymentStatus() == PaymentStatus.ENCAISSE || payment.getPaymentStatus() == PaymentStatus.REJETE) {
+            throw new ResourceNotFoundException("payment can not be processed");
+        }
+        Order order = orderRepository.findById(payment.getOrder().getId()).orElseThrow(() -> {
+            throw new ResourceNotFoundException("there is no order with ID" + payment.getOrder().getId());
+        });
+        if (order.getRemainingAmount() - dto.getAmount() < 0) {
+            throw new BusinessRuleViolationException("payment amount exceeds the order remains  ");
+        }
+        payment.setAmount(payment.getAmount()+ dto.getAmount());
+        order.setRemainingAmount(order.getRemainingAmount()- dto.getAmount() );
+        orderRepository.save(order);
+        paymentRepository.save(payment);
+
         return paymentMapper.toResponse(payment);
     }
 }
