ba9b73143aaf5fea4a4ce521628aa516388198a0
youssefElhoubi <elhoubiyoussef@gmail.com>
2025-11-28 10:52:28 +0100

    Implement Payment controller with add and update payment endpoints

---
+++ src/main/java/com/smartshop/controller/Payment.java
@@ -0,0 +1,30 @@
+package com.smartshop.controller;
+
+import com.smartshop.dto.payment.PaymentRequestDTO;
+import com.smartshop.dto.payment.PaymentResponseDTO;
+import com.smartshop.dto.payment.PaymentUpdateDTO;
+import com.smartshop.service.PaymentService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/payment")
+public class Payment {
+    private final PaymentService paymentService;
+
+    public Payment(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> addPayment(@Valid @RequestBody PaymentRequestDTO request){
+        PaymentResponseDTO response = paymentService.addPayment(request);
+        return ResponseEntity.ok(response);
+    }
+    @PatchMapping("/{id}")
+    public ResponseEntity<?> updatePayment(@Valid @RequestBody PaymentUpdateDTO request,@PathVariable Long id){
+        PaymentResponseDTO response = paymentService.updatePayment(request,id);
+        return ResponseEntity.ok(response);
+    }
+}
